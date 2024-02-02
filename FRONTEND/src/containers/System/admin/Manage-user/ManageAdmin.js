b15caa363a0532c7c252add44674dcfedbf7c6a8
PhamPhong13 <ptphonggithub@gmail.com>
2024-02-02 20:50:48 +0700

    search admin 9:00pm 2/2

--- FRONTEND/src/containers/System/admin/Manage-user/ManageAdmin.js
+++ FRONTEND/src/containers/System/admin/Manage-user/ManageAdmin.js
@@ -55,6 +55,111 @@ class ManageAdmin extends Component
             else toast.error("Xóa người dùng mới không thành công!");
     }
 
+    // bỏ dấu trong chuổi
+    removedau = (word) => {
+        var dauMap = {
+        'à': 'a', 'á': 'a', 'ả': 'a', 'ã': 'a', 'ạ': 'a',
+        'ă': 'a', 'ằ': 'a', 'ắ': 'a', 'ẳ': 'a', 'ẵ': 'a', 'ặ': 'a',
+        'â': 'a', 'ầ': 'a', 'ấ': 'a', 'ẩ': 'a', 'ẫ': 'a', 'ậ': 'a',
+        'đ': 'd',
+        'è': 'e', 'é': 'e', 'ẻ': 'e', 'ẽ': 'e', 'ẹ': 'e',
+        'ê': 'e', 'ề': 'e', 'ế': 'e', 'ể': 'e', 'ễ': 'e', 'ệ': 'e',
+        'ì': 'i', 'í': 'i', 'ỉ': 'i', 'ĩ': 'i', 'ị': 'i',
+        'ò': 'o', 'ó': 'o', 'ỏ': 'o', 'õ': 'o', 'ọ': 'o',
+        'ô': 'o', 'ồ': 'o', 'ố': 'o', 'ổ': 'o', 'ỗ': 'o', 'ộ': 'o',
+        'ơ': 'o', 'ờ': 'o', 'ớ': 'o', 'ở': 'o', 'ỡ': 'o', 'ợ': 'o',
+        'ù': 'u', 'ú': 'u', 'ủ': 'u', 'ũ': 'u', 'ụ': 'u',
+        'ư': 'u', 'ừ': 'u', 'ứ': 'u', 'ử': 'u', 'ữ': 'u', 'ự': 'u',
+        'ỳ': 'y', 'ý': 'y', 'ỷ': 'y', 'ỹ': 'y', 'ỵ': 'y',
+        'À': 'A', 'Á': 'A', 'Ả': 'A', 'Ã': 'A', 'Ạ': 'A',
+        'Ă': 'A', 'Ằ': 'A', 'Ắ': 'A', 'Ẳ': 'A', 'Ẵ': 'A', 'Ặ': 'A',
+        'Â': 'A', 'Ầ': 'A', 'Ấ': 'A', 'Ẩ': 'A', 'Ẫ': 'A', 'Ậ': 'A',
+        'Đ': 'D',
+        'È': 'E', 'É': 'E', 'Ẻ': 'E', 'Ẽ': 'E', 'Ẹ': 'E',
+        'Ê': 'E', 'Ề': 'E', 'Ế': 'E', 'Ể': 'E', 'Ễ': 'E', 'Ệ': 'E',
+        'Ì': 'I', 'Í': 'I', 'Ỉ': 'I', 'Ĩ': 'I', 'Ị': 'I',
+        'Ò': 'O', 'Ó': 'O', 'Ỏ': 'O', 'Õ': 'O', 'Ọ': 'O',
+        'Ô': 'O', 'Ồ': 'O', 'Ố': 'O', 'Ổ': 'O', 'Ỗ': 'O', 'Ộ': 'O',
+        'Ơ': 'O', 'Ờ': 'O', 'Ớ': 'O', 'Ở': 'O', 'Ỡ': 'O', 'Ợ': 'O',
+        'Ù': 'U', 'Ú': 'U', 'Ủ': 'U', 'Ũ': 'U', 'Ụ': 'U',
+        'Ư': 'U', 'Ừ': 'U', 'Ứ': 'U', 'Ử': 'U', 'Ữ': 'U', 'Ự': 'U',
+        'Ỳ': 'Y', 'Ý': 'Y', 'Ỷ': 'Y', 'Ỹ': 'Y', 'Ỵ': 'Y'
+    };
+
+    return word.replace(/[^A-Za-z0-9]/g, function(x) { return dauMap[x] || x; });
+    } 
+
+    formatKeySearch = (key) => {
+        return this.removedau(key).toLowerCase().replace(/\s+/g, ' ').trim()
+    }
+
+
+
+    handleOchangeToSearch = async(event) => {
+
+        if (event.target.value.length <= 0) {
+            this.getAllAdmins();
+        }
+        else {
+            let key = this.removedau(event.target.value).toLowerCase();
+            let res =  await getAllAdmin();
+            let result = [];
+            res.data.map((item, index) => {
+                let fullName = item.fullName.toLowerCase()
+                let name = this.removedau(fullName);
+                if (name.includes(key) === true) {
+                    result.push(item);
+                }
+            })
+
+            if (result.length > 0) {
+                this.setState({
+                    listAdmin: result
+                })
+            
+            }
+            else {
+
+                res.data.map((item, index) => {
+                let name = this.removedau(item.phone);
+                    if (name.includes(key) === true) {
+                        result.push(item);
+                    }
+                })
+                if (result.length > 0) {
+                    this.setState({
+                        listAdmin: result
+                    })            
+                }
+                else {
+
+                    res.data.map((item, index) => {
+                        let email = item.email.toLowerCase()
+                        let name = this.removedau(email);
+                        if (name.includes(key) === true) {
+                            result.push(item);
+                        }
+                    })
+
+                    if (result.length > 0) {
+                        this.setState({
+                            listAdmin: result
+                        })
+            
+                    }  
+                    else {
+                        this.setState({
+                            listAdmin: []
+                        })
+                    }
+
+                }
+                
+            }
+
+        }
+    }
+
     render ()
     {
 
@@ -70,14 +175,17 @@ class ManageAdmin extends Component
 
                     <div className='search'>
                         <div className='form-search'>
-                            <input type="text" placeholder={this.props.language === 'vi' ? "Tên người dùng": "User Name"} />
+                            <input type="text"
+                                placeholder={this.props.language === 'vi' ? "Tên người dùng" : "User Name"}
+                                onChange={(event) => this.handleOchangeToSearch(event)}
+                            />
                             <i className='fas fa-search'></i>
                         </div>
                     </div>
 
                     <div className='btn-add'>
                         <div className='btn btn-primary'
-                        onClick={() => this.linkToAddAdmin()}
+                            onClick={() => this.linkToAddAdmin()}
                         >
                             + Thêm tài khoản
                         </div>
