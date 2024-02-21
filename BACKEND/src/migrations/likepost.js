36855f7c3362bc91d86feb3d41b843115eed5129
PhamPhong13 <ptphonggithub@gmail.com>
2024-02-21 20:40:21 +0700

    tạo bảng like post

---
+++ BACKEND/src/migrations/likepost.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+    up: async ( queryInterface, Sequelize ) =>
+    {
+        await queryInterface.createTable( 'likeposts', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            userId: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            postId: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        } );
+    },
+    down: async ( queryInterface, Sequelize ) =>
+    {
+        await queryInterface.dropTable( 'likeposts' );
+    }
+};
